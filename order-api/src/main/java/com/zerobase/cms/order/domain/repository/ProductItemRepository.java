f7ba431e325fbc4a8eece206d59820da7f1e3a04
Dongvin,Park <dongvin99@naver.com>
2023-02-04 16:19:54 +0900

    상품 검색 및 상품 디테일 검색 구현 완료.

--- order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductItemRepository.java
+++ order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductItemRepository.java
@@ -2,7 +2,9 @@ package com.zerobase.cms.order.domain.repository;
 
 import com.zerobase.cms.order.domain.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
 
 }
